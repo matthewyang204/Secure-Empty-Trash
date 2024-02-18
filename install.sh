07789c2a29ed1549f74cb876b0a46877585555fd
Matthew Yang <141765903+matthewyang204@users.noreply.github.com>
2024-02-18 12:24:44 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ echo "Downloading..."
 curl -LO https://github.com/matthewyang204/Secure-Empty-Trash/blob/main/Secure%20Empty%20Trash%20Installer.dmg
 echo "Installing..."
 hdiutil mount ~/Downloads/Secure Empty Trash Installer.dmg
-cp -R /Volumes/Secure Empty Trash Installer /usr/local/bin/secureemptytrash
+echo "Installing with root, enter password if prompted"
+sudo cp /Volumes/Secure Empty Trash Installer /usr/local/bin/secureemptytrash
 hdiutil detach disk5
 echo "Installation complete"
